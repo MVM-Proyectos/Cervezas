2454805800344b9cfa207b17e71a896dc7473364
RodrigoMahalufRecasens <rmahaluf@gmail.com>
2018-09-06 12:37:18 -0300

    iniciando el proyecto

---
+++ MVM_Cervezas/MVM_Cervezas/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for MVM_Cervezas project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MVM_Cervezas.settings")
+
+application = get_wsgi_application()
